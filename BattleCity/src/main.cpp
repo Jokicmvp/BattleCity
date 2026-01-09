f9139beb273a8f8f842b78eecedbcdc1419262b3
jokic <jokicmvp15@gmail.com>
2026-01-10 01:45:34 +0500

    Draw triangle and added ShaderProgram class

--- BattleCity/src/main.cpp
+++ BattleCity/src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "Renderer/ShaderProgram.h"
 
 GLfloat point[] = {
     0.0f, 0.5f, 0.0f,
@@ -10,25 +11,25 @@ GLfloat point[] = {
 
 GLfloat colors[] = {
     1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
+    1.0f, 0.0f, 0.0f,
+    1.0f, 0.0f, 0.0f
 };
 
 const char* vertex_shader =
 "#version 460\n"
 "layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 color_position;"
+"layout(location = 1) in vec3 vertex_color;"
 "out vec3 color;"
-"int main() {"
+"void main() {"
 "   color = vertex_color;"
 "   gl_Position = vec4(vertex_position, 1.0);"
 "}";
 
 const char* fragment_shader =
 "#version 460\n"
-"in vec3 color"
-"out vec4 frag_color"
-"int main() {"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main() {"
 "   frag_color = vec4(color, 1.0);"
 "}";
 
@@ -88,21 +89,13 @@ int main(void)
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     glClearColor(0, 1, 1, 1);
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertex_shader, nullptr);
-    glCompileShader(vs);
-
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragment_shader, nullptr);
-    glCompileShader(fs);
-
-    GLuint shader_program = glCreateProgram();
-    glAttachShader(shader_program, vs);
-    glAttachShader(shader_program, fs);
-    glLinkProgram(shader_program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    std::string vertexShader(vertex_shader);
+    std::string fragmentShader(fragment_shader);
+    Renderer::ShaderProgram shaderProgram(vertex_shader, fragment_shader);
+    if (!shaderProgram.isCompiled())
+    {
+        std::cout << "Can't create shader program!" << std::endl;
+    }
 
     GLuint points_vbo = 0;
     glGenBuffers(1, &points_vbo);
@@ -126,20 +119,16 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
+        shaderProgram.use();
         glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
